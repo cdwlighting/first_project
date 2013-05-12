d108da536230a2b6de09c90dee6ae176056c8667
Chris Warner <cdw.lighting@gmail.com>
2013-05-11 23:45:21 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstProject::Application.config.secret_token = 'a7217e4801d0e0be28b11950d330abfe32fb3414b42de1de63e558d8c053e8ef7cb574dc661259278fa8d02221332a35cf60fa7442f4cd3a430ead1b3a0b806c'
